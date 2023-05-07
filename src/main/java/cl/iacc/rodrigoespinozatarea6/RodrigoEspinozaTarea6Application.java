dd4cd853b2211dac753e9f63b96810c228328793
Rodrigo Espinoza <reespinoza@falabella.cl>
2023-05-07 19:16:47 -0400

    including assignments

--- src/main/java/cl/iacc/rodrigoespinozatarea6/RodrigoEspinozaTarea6Application.java
+++ src/main/java/cl/iacc/rodrigoespinozatarea6/RodrigoEspinozaTarea6Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RodrigoEspinozaTarea6Application {
 
+	// The main method starts the Tomcat server self-contained in the app.
 	public static void main(String[] args) {
 		SpringApplication.run(RodrigoEspinozaTarea6Application.class, args);
 	}
